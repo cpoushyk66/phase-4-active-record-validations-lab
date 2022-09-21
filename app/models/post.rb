8801ceab2f8cd7ecfcea7bcffb3268adc7273667
Camilla Poushyk <camilla.poushyk@gmail.com>
2022-09-21 11:15:54 -0400

    commit tree f1fb8b895ac60680f4fa0eedb681e1c447ab541e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, { :length => { :minimum => 250 } }
+    validates :summary, { :length => { :maximum => 250 } }
+    validates :category, { :inclusion => {in: %w(Fiction Non-Fiction)}}
+    validate :click_baity
+
+
+
+    
+    def click_baity
+
+        if (title != nil && !(["Won't Believe", "Secret", "Top [number]", "Guess"].any? { |check| title.include?(check) })) 
+            errors.add(:title, "Sorry, not click baity enough!")
+        end
+    end
+
+
+    
 end
